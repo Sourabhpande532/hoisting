e34a72f0e51690560282a8c3a7ceac794e7e290e
sourabh <sourabhpande43@gmail.comm>
2023-02-08 15:58:31 +0530

    add some plugins part

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,8 @@
-// /* eslint-disable */
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+/* eslint-disable */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:8000',
-//       },
-//     },
-//   },
-// });
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
