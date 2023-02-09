ec2a1de83a56e94adca556d785c290e3314f6239
Aditya Kadali <adityakadali00@gmail.com>
2023-02-09 19:22:36 +0530

    Fix: CORs Access Control Allow Origin

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 8000;
 /* Middleware */
 
 app.use(cors({
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials : true,
   Origin : "https://hoisting-eight.vercel.app"
 }));
